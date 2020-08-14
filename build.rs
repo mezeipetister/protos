84a946fc8363d36c017d96aa1a4c3e3deb20f8ad
Peter <mezeipetister@gmail.com>
2020-08-14 18:19:51 +0200

    ..

--- build.rs
+++ build.rs
@@ -1,7 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure().out_dir("src/proto/").compile(
-        &["src/proto/hello.proto", "src/proto/hello2.proto"],
-        &["src/proto"],
-    )?;
+    tonic_build::configure()
+        .out_dir("src/proto/")
+        .compile(&["src/proto/user.proto"], &["src/proto"])?;
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,2 @@
 pub mod proto;
-pub use proto;
+pub use proto::*;
